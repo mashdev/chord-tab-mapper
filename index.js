2497a0f87b3a95dc700cfc1f7e57eab5b6c0227c
Paul Lopez <paullopez05@gmail.com>
2019-11-11 23:08:08 -0700

    created array with empty array line

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ function getLyrics() {
     //count number of lines
     let linecount = value.split("\n")
     var arr = [];
+    var emptyLine = [];
     var newline = [];
-    
+
+    // create an array of arrays with an empty line
     for(i=0; i<linecount.length; i++){
-      if(linecount[i].split(" ") || linecount[i].split("  "))
-        newline[i].push("-")
+        len = linecount[i].split(" ").length
+        arr.push(Array.apply(null, {length : len}))
         arr.push(linecount[i].split(" "))
      }
-    console.log(arr);
 
+    console.log(arr);
     render.innerText = arr;
-    // console.log(arr);
-    
 }
